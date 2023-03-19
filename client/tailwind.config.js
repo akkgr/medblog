ef5ca64612f504ef6d833ce4eb6a86f56dd18471
anastasios.kanellopoulos <anastasios.kanellopoulos@fenergo.com>
2023-03-19 19:22:23 +0200

    client init

---
+++ client/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    '!**/{bin,obj,node_modules}/**',
+    '**/*.{razor,html,cshtml}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
